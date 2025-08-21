8359e83b60128093ef62cdc935cc99cfa10ee4a2
Joaov-cardoso <joaovcardosom@gmail.com>
2025-08-21 10:41:01 -0300

    Exercicios

--- Exercicios 19-08-25 (While)/Lista 01/exercicios.js
+++ Exercicios 19-08-25 (While)/Lista 01/exercicios.js
@@ -1,9 +1,9 @@
-function exercicio1(){
+function exercicio1() {
     let indice = 0;
     let precoTotal = 0;
-    while(indice < 4){
+    while (indice < 4) {
         let precos = parseFloat(prompt("Digite os valores:").replace(",", "."))
-        
+
         precoTotal = precoTotal + precos
         indice = indice + 1
     }
@@ -13,41 +13,41 @@ function exercicio1(){
     console.log("Preço Total: " + precoTotal + "\nMédia: " + media)
 }
 
-function exercicio2(){
+function exercicio2() {
     let indice = 0;
     let comecaComA = 0, outrosComecos = 0;
 
-    while(indice < 5){
+    while (indice < 5) {
         let palavras = prompt("Digite as palavras:")
 
-        if (palavras.toLowerCase().trim().startsWith("a")){
+        if (palavras.toLowerCase().trim().startsWith("a")) {
             comecaComA = comecaComA + 1
-        }else{
+        } else {
             outrosComecos = outrosComecos + 1
         }
-        
+
         indice = indice + 1
     }
     console.log("Quantidade que começa com 'A': " + comecaComA + "\nOutras palavras: " + outrosComecos)
 }
 
-function exercicio3(){
+function exercicio3() {
     let idade = 0;
-    while(idade < 1 || idade > 120){
+    while (idade < 1 || idade > 120) {
         idade = parseInt(prompt("Digite a idade"));
 
-        if(idade < 1){
+        if (idade < 1) {
             alert("Idade inválida, digite novamente")
-        }else if(idade > 120){
+        } else if (idade > 120) {
             alert("Idade inválida, digite novamente")
-        }else{
+        } else {
             alert("Idade cadastrada: " + idade)
         }
 
     }
 }
 
-function exercicio4(){
+function exercicio4() {
     let indice = 0;
     let maiorAltura = 0;
     while (indice < 3) {
@@ -62,7 +62,7 @@ function exercicio4(){
     alert("Maior Altura: " + maiorAltura.toFixed(2));
 }
 
-function exercicio5(){
+function exercicio5() {
     let menu = `Menu:
     1 - Saudação
     2 - Par ou ímpar
@@ -70,36 +70,36 @@ function exercicio5(){
     100 - Sair
     Digite a opção`
     let opcaoEscolhida = 0;
-    while (opcaoEscolhida !== 100){
+    while (opcaoEscolhida !== 100) {
         opcaoEscolhida = parseInt(prompt(menu));
-        
-        if(opcaoEscolhida === 1){
+
+        if (opcaoEscolhida === 1) {
             let nome = prompt("Digite seu nome");
             alert("Olá, " + nome)
-        }else if(opcaoEscolhida === 2){
+        } else if (opcaoEscolhida === 2) {
             let numero = parseInt(prompt("Digite um número"));
-            if (numero % 2 === 0){
+            if (numero % 2 === 0) {
                 alert("O número " + numero + " é par")
-            }else{
+            } else {
                 alert("O número " + numero + " é impar")
             }
-        }else if(opcaoEscolhida !== 100){
+        } else if (opcaoEscolhida !== 100) {
             alert("Opção inválida")
-        }else{
+        } else {
             alert("Sistema encerrado")
         }
     }
 }
 
-function exercicio6(){
+function exercicio6() {
     let indice = 0;
     let nome = "";
-    let nomesRegistrados = "" ;
+    let nomesRegistrados = "";
 
     while (nome.toLowerCase().trim() !== "fim") {
         nome = prompt("Digite o nome ou 'fim' para encerrar");
 
-        if(nome.toLowerCase().trim() !== "fim"){
+        if (nome.toLowerCase().trim() !== "fim") {
             nomesRegistrados = nomesRegistrados + nome + "\n"
             indice = indice + 1
         }
@@ -107,10 +107,10 @@ function exercicio6(){
     console.log("Nomes registrados: " + nomesRegistrados + "\nQuantidades de nome: " + indice)
 }
 
-function exercicio7(){
+function exercicio7() {
     let indice = 0;
     let precosTotal = 0;
-    while(indice < 4){
+    while (indice < 4) {
         let preco = parseFloat(prompt("Digite o valor").replace("R$", "").replace(" ", "").replace(",", "."));
 
 
@@ -120,4 +120,59 @@ function exercicio7(){
     let media = precosTotal / indice
 
     console.log("Soma dos preços: " + precosTotal.toFixed(2) + "\nMédia dos preços: " + media.toFixed(2))
+}
+
+function exercicio8() {
+    let indice = 0;
+    let perguntas = 0, naoPerguntas = 0;
+    while (indice < 5) {
+        let frases = prompt("Digite uma pergunta ou uma afirmação.")
+
+        if (frases.trimEnd().endsWith("?")) {
+            perguntas = perguntas + 1
+        } else {
+            naoPerguntas = naoPerguntas + 1
+        }
+        indice = indice + 1
+    }
+
+    console.log("Quantidade de perguntas: " + perguntas + "\nQuantidade de não perguntas: " + naoPerguntas)
+}
+
+function exercicio9() {
+    let indice = 0;
+    let quantidadeVip = 0;
+    let quantidadeOff = 0;
+    let outrosCupons = 0;
+    while (indice < 5) {
+        let cupom = prompt("Digite os cupons");
+
+        if (cupom.toLowerCase().trim().startsWith("vip")) {
+            quantidadeVip = quantidadeVip + 1
+        } else if (cupom.toLowerCase().trim().endsWith("off")) {
+            quantidadeOff = quantidadeOff + 1
+        } else {
+            outrosCupons = outrosCupons + 1
+        }
+
+        indice = indice + 1
+    }
+
+    console.log("Cupons VIP: " + quantidadeVip + "\nCupons OFF: " + quantidadeOff + "\nOutros cupons: " + outrosCupons)
+}
+
+function exercicio10() {
+    let indice = 0;
+    let nomeCompleto = "";
+    let nomesRegistrados = "";
+    while (indice < 3) {
+        nomeCompleto = prompt("Digite o nome completo").trim();
+        let partesNome = nomeCompleto.split(" ");
+        let primeiroNome = partesNome[0];
+
+        nomesRegistrados = nomesRegistrados + primeiroNome + "\n"
+
+        indice = indice + 1
+    }
+    console.log("Primeiros nomes: " + nomesRegistrados)
 }
---
+++ Exercicios 19-08-25 (While)/Lista 02/exercicios.js
@@ -0,0 +1,19 @@
+function exercicio1(){
+    let desejaCadastrar = "sim";
+    let precoTotal = 0;
+    let quantidade = 0;
+    let maiorPreco = 0;
+    let nomeMaisCaro = "";
+    while(desejaCadastrar === "sim"){
+        let produto = prompt("Digite o produto");
+        let preco = parseFloat(prompt("Digite o preço").replace(" ", "").replace("R$", "").replace(",", "."));
+
+        while(produto.length >= 2){
+            
+        }
+
+
+
+
+    }
+}
